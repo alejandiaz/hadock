5900b1c06d2ccc190a9e578c74da50dac9516de5
nico <brun.nicolas21@gmail.com>
2018-08-09 12:23:38 +0200

    use channels to send request for new conn and another one to get the new writer

--- proxy.go
+++ proxy.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"io/ioutil"
 	"net"
-	"sync"
 	"time"
 )
 
@@ -15,80 +14,125 @@ type flusher interface {
 }
 
 type proxy struct {
-	level string
+	send chan io.WriteCloser
+	recv <-chan io.WriteCloser
 
-	mu sync.Mutex
-	net.Conn
-	inner io.Writer
+	writer io.WriteCloser
 }
 
 func DialProxy(addr, level string) (io.WriteCloser, error) {
 	if addr == "" {
 		return nil, fmt.Errorf("empty address")
 	}
-	p := proxy{inner: ioutil.Discard, level: level}
-	go p.reset(addr)
+	p := proxy{
+		send:   make(chan io.WriteCloser),
+		writer: Discard(),
+	}
+	p.recv = reset(p.send, addr, level)
+	p.send <- nil
 	return &p, nil
 }
 
 func (p *proxy) Write(bs []byte) (int, error) {
-	p.mu.Lock()
-	defer p.mu.Unlock()
+	select {
+	case w, ok := <-p.recv:
+		if !ok {
+			return 0, io.EOF
+		}
+		p.writer = w
+	default:
+	}
 
-	_, err := p.inner.Write(bs)
-	f, ok := p.inner.(flusher)
+	_, err := p.writer.Write(bs)
+	f, ok := p.writer.(flusher)
 	if err == nil && ok {
 		err = f.Flush()
 	}
 
-	if err != nil {
-		p.inner = ioutil.Discard
-		p.Conn.Close()
-		go p.reset(p.Conn.RemoteAddr().String())
+	if _, ok := p.writer.(*discarder); err != nil || ok {
+		p.send <- p.writer
+		p.writer = Discard()
 	}
 
 	return len(bs), nil
 }
 
 func (p *proxy) Close() error {
-	if p.Conn == nil {
-		return nil
-	}
-	if f, ok := p.inner.(flusher); ok {
+	if f, ok := p.writer.(flusher); ok && p.writer != nil {
 		f.Flush()
 	}
-	return p.Conn.Close()
-}
-
-func (p *proxy) reset(addr string) {
-	p.inner = ioutil.Discard
-	for {
-		c, err := net.DialTimeout("tcp", addr, time.Second*5)
-		if err == nil {
-			p.mu.Lock()
-			defer p.mu.Unlock()
-
-			p.Conn, p.inner = c, c
-
-			var level int
-			deflate := true
-			switch p.level {
-			default:
-				deflate = false
-			case "no":
-				level = gzip.NoCompression
-			case "speed":
-				level = gzip.BestSpeed
-			case "best":
-				level = gzip.BestCompression
-			case "default":
-				level = gzip.DefaultCompression
-			}
-			if deflate {
-				p.inner, _ = gzip.NewWriterLevel(p.inner, level)
-			}
-			return
+	select {
+	case _, ok := <-p.send:
+		if !ok {
+			return io.EOF
 		}
-		time.Sleep(time.Millisecond * 100)
+	default:
 	}
+	close(p.send)
+	return p.writer.Close()
 }
+
+func reset(in <-chan io.WriteCloser, addr, level string) <-chan io.WriteCloser {
+	queue := make(chan io.WriteCloser)
+	go func() {
+		defer close(queue)
+
+		sema := make(chan struct{}, 1)
+		for {
+			wc, ok := <-in
+			if !ok {
+				return
+			}
+			select {
+			case sema <- struct{}{}:
+			default:
+				continue
+			}
+			if wc != nil {
+				wc.Close()
+			}
+			go func() {
+				for {
+					c, err := net.DialTimeout("tcp", addr, time.Second*5)
+					if err != nil {
+						time.Sleep(250 * time.Millisecond)
+						continue
+					}
+					var w io.WriteCloser = c
+
+					var lvl int
+					deflate := true
+					switch level {
+					default:
+						deflate = false
+					case "no":
+						lvl = gzip.NoCompression
+					case "speed":
+						lvl = gzip.BestSpeed
+					case "best":
+						lvl = gzip.BestCompression
+					case "default":
+						lvl = gzip.DefaultCompression
+					}
+					if deflate {
+						w, _ = gzip.NewWriterLevel(w, lvl)
+					}
+					queue <- w
+					break
+				}
+				<-sema
+			}()
+		}
+	}()
+	return queue
+}
+
+type discarder struct {
+	io.Writer
+}
+
+func Discard() io.WriteCloser {
+	return &discarder{ioutil.Discard}
+}
+
+func (_ discarder) Close() error { return nil }
